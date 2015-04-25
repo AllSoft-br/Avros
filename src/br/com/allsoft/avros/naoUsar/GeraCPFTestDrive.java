b126bc738cf8d617b8cd2914e1bec21d83bbbd96
Luana Nascimento <lluana13@hotmail.com>
2015-04-24 21:09:13 -0300

    Classes de teste

---
+++ src/br/com/allsoft/avros/naoUsar/GeraCPFTestDrive.java
@@ -0,0 +1,13 @@
+package br.com.allsoft.avros.naoUsar;
+
+/**
+ * @author lucas marques
+ * */
+
+public class GeraCPFTestDrive {
+	public static void main(String[] args){
+		GeraCPF g = new GeraCPF();
+		
+		System.out.println(g.geraCPFFinal());
+	} 
+}
